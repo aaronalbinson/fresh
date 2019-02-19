b4b5e5e062518232d1e006e91f9670c567c3fc37
Aaron <azza1990uk@yahoo.com>
2019-02-19 01:42:29 +0000

    Add instagram and increase footer height

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -26,6 +26,7 @@ const TemplateWrapper = ({ children }) => (
                 sociallinks {
                   facebook
                   twitter
+                  instagram
                 }
               }
             }
