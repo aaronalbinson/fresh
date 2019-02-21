a2cd505269ae1d28bfafab188a9c9a44f9b38c6c
Aaron <azza1990uk@yahoo.com>
2019-02-21 23:52:35 +0000

    Remove classes from judges

--- src/components/Judges/Judges.js
+++ src/components/Judges/Judges.js
@@ -15,11 +15,11 @@ class Judges extends React.Component {
       <div className="judges">
         <div className="">
           <div className="columns">
-            <div className="column is-8">
+            <div className="column">
               <div className="content">
                 <div className="columns is-multiline">
                   {this.props.judges.map(judge => (
-                    <div className="column is-3">
+                    <div className="judge">
                       <div class="content">
                         <h3>{judge.judgename}</h3>
                         <h4>{judge.judgecompany}</h4>
